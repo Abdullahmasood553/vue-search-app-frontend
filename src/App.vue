e79e05648e180fa5cecfdaa76a897a5a149b820a
abdullahmasood553 <abdullahmasood553@gmail.com>
2021-10-24 00:55:11 -0700

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 m-auto mt-4">
+        <div class="form-group">
+          <input type="text" v-model="keyword" class="form-control form-control-lg" placeholder="Search Product">
+          <div v-if="products?.length > 0">
+            <div v-for="product in products" :key="product.id">
+              <div class="card card-body mb-1">
+                <h6 class="text-white" v-text="product.title"></h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import axios from 'axios';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {},
+    data() {
+      return {
+        keyword: '',
+        products: Array,
+      }
+    },
+    watch: {
+      keyword: function () {
+        this.getResults()
+      }
+    },
+    methods: {
+      getResults: function () {
+        axios.get('http://localhost:8000/api/products/backend?search=' + this.keyword)
+          .then(res => this.products = res.data)
+          .catch(error => {
+            console.log(error);
+          });
+      }
+    },
+    mounted() {
+      console.log('Search Component');
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
